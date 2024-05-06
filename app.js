fd004980739c80c1ae9a6d236988413f4722bde4
Marco Krooß <zkamekyu@gmail.com>
2024-05-06 16:28:38 +0000

    refactored functions; commented code

--- app.js
+++ app.js
@@ -21,22 +21,36 @@ function createLobby(id, name, password, status, players) {
     return new Lobby(id, name, password, status, players);
 }
 
-function initPlayer() {
+// createPlayer-Function | 
+function createPlayer(id, name, playerCount, score) {
     return new Player(id, name, playerCount, score);
 }
 
-// function hostGame() {
+function joinLobby(x, y) {
+    //TODO: Implement lobby joining function
+    //TODO: Player y should join Lobby x
+}
 
-// }
+function startGame() {
+    //TODO: start the game (get lobby on the board)
 
-// function switchPlayer() {
+    //TODO: add online multiplayer functionality
+}
 
-// }
+function switchPlayer() {
+    //TODO: cycle through the player after each playerTurn()
+}
+
+function playerTurn() {
+    //TODO: player makes turn (role the dice, move player (automatically), if X then player gets 2nd turn)
+    let dice1 = roleDice();
+    let dice2 = roleDice();
+}
 
 function roleDice() {
+    // random number 1 - 6
     return Math.ceil(Math.random() * 6);
 }
 
-// function playerTurn() {
-
-// }
+//? later on "CrazyMode" where there are more than 2 dice and numbers from 1-12 or from 1-3/3-6 etc.
+//? maybe implement this with powerups?
